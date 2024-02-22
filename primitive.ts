c85497424a4c22bc53adbe551eae265d4433e555
naisannovel <naisannovel@gmail.com>
2024-02-22 23:08:07 +0600

    update

--- primitive.ts
+++
@@ -1,9 +0,0 @@
-const str1: string = 'naisan';
-
-const num1: number = 1;
-
-const strOrNum1: string | number = 2;
-
-let strOrNum2: string | number;
-    strOrNum2 = 'naisan';
-    strOrNum2 = 2;
--- tuple.ts
+++
@@ -1,3 +0,0 @@
-const student1 :[number, string] = [1, 'student'];
-const student2 :[number, string] = [2, 'student'];
-const student3 :[number, string] = [3, 'student'];
